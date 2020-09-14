602127470e5507d2e1b426c0772b23b517121d0f
Chris Talkington <chris@talkingtontech.com>
2020-09-13 23:52:51 -0500

    Update __init__.py

--- homeassistant/components/canary/__init__.py
+++ homeassistant/components/canary/__init__.py
@@ -6,20 +6,20 @@ from canary.api import Api
 from requests import ConnectTimeout, HTTPError
 import voluptuous as vol
 
+from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
 from homeassistant.const import CONF_PASSWORD, CONF_TIMEOUT, CONF_USERNAME
+from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers import discovery
 import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.typing import HomeAssistantType
 from homeassistant.util import Throttle
+
+from .const import DEFAULT_TIMEOUT, DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-NOTIFICATION_ID = "canary_notification"
-NOTIFICATION_TITLE = "Canary Setup"
-
-DOMAIN = "canary"
 DATA_CANARY = "canary"
 MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=30)
-DEFAULT_TIMEOUT = 10
 
 CONFIG_SCHEMA = vol.Schema(
     {
@@ -34,31 +34,65 @@ CONFIG_SCHEMA = vol.Schema(
     extra=vol.ALLOW_EXTRA,
 )
 
-CANARY_COMPONENTS = ["alarm_control_panel", "camera", "sensor"]
+PLATFORMS = ["alarm_control_panel", "camera", "sensor"]
 
 
-def setup(hass, config):
-    """Set up the Canary component."""
-    conf = config[DOMAIN]
-    username = conf[CONF_USERNAME]
-    password = conf[CONF_PASSWORD]
-    timeout = conf[CONF_TIMEOUT]
+async def async_setup(hass: HomeAssistantType, config: dict) -> bool:
+    """Set up the Canary integration."""
+    hass.data.setdefault(DOMAIN, {})
 
-    try:
-        hass.data[DATA_CANARY] = CanaryData(username, password, timeout)
-    except (ConnectTimeout, HTTPError) as ex:
-        _LOGGER.error("Unable to connect to Canary service: %s", str(ex))
-        hass.components.persistent_notification.create(
-            f"Error: {ex}<br />You will need to restart hass after fixing.",
-            title=NOTIFICATION_TITLE,
-            notification_id=NOTIFICATION_ID,
+    if hass.config_entries.async_entries(DOMAIN):
+        return True
+
+    if DOMAIN in config:
+        hass.async_create_task(
+            hass.config_entries.flow.async_init(
+                DOMAIN,
+                context={"source": SOURCE_IMPORT},
+                data=config[DOMAIN],
+            )
         )
-        return False
-
-    for component in CANARY_COMPONENTS:
-        discovery.load_platform(hass, component, DOMAIN, {}, config)
 
     return True
+
+
+async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry) -> bool:
+    """Set up Canary from a config entry."""
+    try:
+        canary_data = CanaryData(username, password, timeout)
+    except ConnectTimeout as error:
+        raise ConfigEntryNotReady from error
+    except HTTPError as error:
+        _LOGGER.error("Unable to connect to Canary service: %s", str(error))
+        return False
+
+    hass.data[DOMAIN][entry.entry_id] = {
+        DATA_CANARY: canary_data,
+    ]
+
+    for component in PLATFORMS:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(entry, component)
+        )
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistantType, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    unload_ok = all(
+        await asyncio.gather(
+            *[
+                hass.config_entries.async_forward_entry_unload(entry, component)
+                for component in PLATFORMS
+            ]
+        )
+    )
+
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
 
 
 class CanaryData:
