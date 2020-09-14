1ef3f79f5e7cd3c4fd5e151d86ad1965fa89a7fd
Chris Talkington <chris@talkingtontech.com>
2020-09-14 00:42:17 -0500

    Update const.py

--- homeassistant/components/canary/const.py
+++ homeassistant/components/canary/const.py
@@ -5,6 +5,10 @@ DOMAIN = "canary"
 # Configuration
 CONF_FFMPEG_ARGUMENTS = "ffmpeg_arguments"
 
+# Data
+DATA_CANARY = "canary"
+DATA_UNDO_UPDATE_LISTENER = "undo_update_listener"
+
 # Defaults
 DEFAULT_FFMPEG_ARGUMENTS = "-pred 1"
 DEFAULT_TIMEOUT = 10
